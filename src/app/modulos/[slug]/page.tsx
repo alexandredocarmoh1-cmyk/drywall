a4d8ee511d38ff352bd7d4587c698adddc44e0e2
Alexandre Do Carmo <alexandredocarmoh1@gmail.com>
2025-11-10 23:00:20 +0000

    Não mudou nada. corrija isso

--- src/app/modulos/[slug]/page.tsx
+++ src/app/modulos/[slug]/page.tsx
@@ -60,7 +60,7 @@ export default function ModulePage() {
               <div key={lesson.id} className="bg-card rounded-lg overflow-hidden shadow-lg transition-transform hover:-translate-y-1">
                 <div className="aspect-video">
                   <iframe
-                    src={`https://www.youtube.com/embed/${lesson.videoId}?rel=0&showinfo=0&autoplay=0`}
+                    src={`https://www.youtube.com/embed/${lesson.videoId}?rel=0&autoplay=0`}
                     allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                     className="w-full h-full"
