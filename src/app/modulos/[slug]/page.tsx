acbafd272e31fc4beade854429abd1febb8f188a
Alexandre Do Carmo <alexandredocarmoh1@gmail.com>
2025-11-10 22:59:16 +0000

    Os vídeos devem exibir somente o conteúdo (sem botões, títulos, sugestõe

--- src/app/modulos/[slug]/page.tsx
+++ src/app/modulos/[slug]/page.tsx
@@ -60,10 +60,11 @@ export default function ModulePage() {
               <div key={lesson.id} className="bg-card rounded-lg overflow-hidden shadow-lg transition-transform hover:-translate-y-1">
                 <div className="aspect-video">
                   <iframe
-                    src={`https://www.youtube.com/embed/${lesson.videoId}`}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    src={`https://www.youtube.com/embed/${lesson.videoId}?rel=0&showinfo=0&autoplay=0`}
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                     className="w-full h-full"
+                    title={lesson.title}
                   ></iframe>
                 </div>
                 <div className="p-4">
